885759422727afd3b6ba432636699369b815caf1
TranQuangHuy21522169 <127572935+HuiiTran@users.noreply.github.com>
2023-07-02 22:12:44 +0700

    Add number into hud

--- DoAnGame/HUD.cpp
+++ DoAnGame/HUD.cpp
@@ -19,20 +19,26 @@ void CHUD::Render()
 	CAnimations::GetInstance()->Get(ID_ANI_HUD)->Render(x, y + HUD_OFFSET_Y);
 	//M SYMBOL
 	CAnimations::GetInstance()->Get(ID_ANI_M_SYMBOL)->Render(x - M_SYMBOL_OFFSET_X, y + M_SYMBOL_OFFSET_Y);
-	//LEVEL RUN EMPTY
+
+
+
+	//LEVEL RUN INCREASE 
 	if(mario->GetLevelRun() > 1)
-		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - 60, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - LEVELRUN_BASE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
 	if (mario->GetLevelRun() > 2)
-		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - 52, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - LEVELRUN_BASE_OFFSET_X + 1 * LEVELRUN_SPACE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
 	if (mario->GetLevelRun() > 3)
-		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - 44, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - LEVELRUN_BASE_OFFSET_X + 2 * LEVELRUN_SPACE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
 	if (mario->GetLevelRun() > 4)
-		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - 36, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - LEVELRUN_BASE_OFFSET_X + 3 * LEVELRUN_SPACE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
 	if (mario->GetLevelRun() > 5)
-		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - 28, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - LEVELRUN_BASE_OFFSET_X + 4 * LEVELRUN_SPACE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
 	if (mario->GetLevelRun() > 6)
-		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - 20, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_ARROW_LEVEL_RUN)->Render(x - LEVELRUN_BASE_OFFSET_X + 5 * LEVELRUN_SPACE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
 	if (mario->GetLevelRun() == LEVEL_RUN_MAX)
-		CAnimations::GetInstance()->Get(ID_ANI_TRIANGLE_LEVEL_RUN)->Render(x - 8, y + 5);
+		CAnimations::GetInstance()->Get(ID_ANI_LEVEL_RUN_MAX)->Render(x - LEVELRUN_SPACE_OFFSET_X, y + LEVELRUN_OFFSET_Y);
+
+	//NUMBER test
+	CAnimations::GetInstance()->Get(ID_ANI_NUMBER_9)->Render(x - 75, y + 5);
 
 }
